4af8c8a8f3a276eb48cf026c32667c52d909e93a
Alan Sampson <alansamps@gmail.com>
2020-03-22 14:22:38 -0700

    Refactor to adopt C++ standard methods of redirecting cout, cerr & cin to files. Add sample data sets for testing.

--- CdtPrdump/prdump.cpp
+++ CdtPrdump/prdump.cpp
@@ -27,39 +27,42 @@
 // ----------------------------------------------------------------------------
 //
 // ----------------------------------------------------------------------------
-#include <cstdlib>
-#include <cstdio>
-#include <string>
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <cstdlib>
+#include <cstdio>
 #include <cctype>
 
-using namespace std;
+//using namespace std;
 
 #include "CompDiff.h"
 
+//  MARK: - Definitions.
 // ----------------------------------------------------------------------------
 //  Local Definitions
 // ----------------------------------------------------------------------------
-#if       defined( _MSC_VER )
-#    pragma comment( exestr, "@(#) -*- prdump.cpp -*- $Header: $ " )
+#if defined( _MSC_VER )
+# pragma comment( exestr, "@(#) -*- prdump.cpp -*- $Header: $ " )
 #endif // defined( _MSC_VER )
 
-static char const * const pszWhat = "@(#) -*- prdump.cpp -*- $Header: $ ";
+static char const * const __unused pszWhat = "@(#) -*- prdump.cpp -*- $Header: $ ";
 
 // ----------------------------------------------------------------------------
 //  Local Prototypes
 // ----------------------------------------------------------------------------
-void           spitUsage( ostream &out );
-char          *FormatOctet( char *iBuf, char *oBuf, int nChar, boolean bWide );
+void           spitUsage( std::ostream &out );
+char          *FormatOctet( char *iBuf, char *oBuf, size_t nChar, boolean bWide );
 inline char   *FormatOffset( char *Offset, unsigned long nOffset );
 inline boolean CheckForSwitch( char *Arg );
 inline boolean IsItNarrow( char *Arg, boolean State );
 inline void    EnvSnoop( char *Arg, char **envp );
 
+//  MARK: - Implementation.
 // ----------------------------------------------------------------------------
 //  Fnuctions
 // ----------------------------------------------------------------------------
+//  MARK: main()
 // ----------------------------------------------------------------------------
 // Function name    : main
 // Description      :
@@ -71,146 +74,163 @@ inline void    EnvSnoop( char *Arg, char **envp );
 // ----------------------------------------------------------------------------
 int main( int argc, char **argv, char **envp )
 {
-  //istream_withassign FTarget( cin );
-  //ostream_withassign FOut( cout );
-    //ostream_withassign FError( cerr );
-  istream FTarget;
-  ostream FOut;
-    ostream FError;
+  std::ifstream FTarget;
+  std::ofstream FOut;
+  std::ofstream FError;
 
-  filebuf buffTarget;
-    filebuf buffOut;
-    filebuf buffError;
+//  std::filebuf buffTarget;
+//  std::filebuf buffOut;
+//  std::filebuf buffError;
+  std::streambuf * cinbuf  = NULL;
+  std::streambuf * coutbuf = NULL;
+  std::streambuf * cerrbuf = NULL;
 
-    boolean bWide = true;
+  boolean bWide = true;
 
-    int    argcLocal   = 0;
-    int    cbargvLocal = argc * sizeof( char * );
-    char **argvLocal   = reinterpret_cast< char ** >( new ( char *[ argc ] ) );
-    memset( argvLocal, 0, cbargvLocal );
+  int    argcLocal   = 0;
+  int    cbargvLocal = argc * sizeof( char * );
+  char **argvLocal   = new char *[ argc ];
+  memset( argvLocal, 0, cbargvLocal );
 
-    if ( argc > 1 )
+  if ( argc > 1 )
+  {
+    for ( int c = 0; c < argc; c++)
     {
-        for ( register int c = 0; c < argc; c++ )
-        {
-            if ( CheckForSwitch( argv[ c ] ) )
-            {
-                bWide = IsItNarrow( argv[ c ], bWide );
-                EnvSnoop( argv[ c ], envp );
-            }
-            else
-            {
-                argvLocal[ argcLocal++ ] = argv[ c ];
-            }
-        }
+      if ( CheckForSwitch( argv[ c ] ) )
+      {
+        bWide = IsItNarrow( argv[ c ], bWide );
+        EnvSnoop( argv[ c ], envp );
+      }
+      else
+      {
+        argvLocal[ argcLocal++ ] = argv[ c ];
+      }
     }
+  }
 
-    bool bError(false);
-    bool bOut(false);
-    bool bTarget(false);
+  bool bError(false);
+  bool bOut(false);
+  bool bTarget(false);
 
-    switch ( argcLocal )
-    {
+  switch ( argcLocal )
+  {
     default:
     case 4:
-        FError  = buffError.open( argvLocal[ 3 ], ios::out );
-        bError  = true;
+//      FError  = buffError.open( argvLocal[ 3 ], std::ios::out );
+      FError  = std::ofstream( argvLocal[ 3 ]);
+      cerrbuf = std::cerr.rdbuf();      //  save system cerr buffer
+      std::cerr.rdbuf(FError.rdbuf());  //  redirect cerr to file
+      bError  = true;
 
     case 3:
-        FOut    = buffOut.open( argvLocal[ 2 ], ios::out );
-        bOut    = true;
+//      FOut    = buffOut.open( argvLocal[ 2 ], std::ios::out );
+      FOut    = std::ofstream( argvLocal[ 2 ]);
+      coutbuf = std::cout.rdbuf();      //  save system cout buffer
+      std::cout.rdbuf(FOut.rdbuf());    //  redirect cout to file
+      bOut    = true;
 
     case 2:
-        FTarget = buffTarget.open( argvLocal[ 1 ], ios::in | ios::binary );
-        bTarget = true;
+//      FTarget = buffTarget.open( argvLocal[ 1 ], std::ios::in | std::ios::binary );
+      FTarget = std::ifstream( argvLocal[ 1 ]);
+      cinbuf  = std::cin.rdbuf();       //  save system cin buffer
+      std::cin.rdbuf(FTarget.rdbuf());  //  redirect cin from file
+      bTarget = true;
 
     case 1:
     case 0:
-        break;
-    }
-    if (!bTarget) {
-      FTarget.rdbuf(cin);
-    }
-    if (!bOut) {
-      FOut.rdbuf(cout);
-    }
-    if (!bError) {
-      FError.rdbuf(cerr);
-    }
+      break;
+  }
+//  if (!bTarget) {
+//    FTarget.rdbuf(std::cin);
+//  }
+//  if (!bOut) {
+//    FOut.rdbuf(std::cout);
+//  }
+//  if (!bError) {
+//    FError.rdbuf(std::cerr);
+//  }
 
-    delete [] argvLocal;
+  delete [] argvLocal;
 
-    unsigned long const cbInBuff  = 4096;
-    unsigned long const cbLineLen = 120;
-    unsigned long const cbLineW   = 32;
-    unsigned long const cbLineN   = 16;
-    unsigned long       cbIn;
-    unsigned long       nBuffOffset = 0;
-    unsigned long       cbLine = bWide ? cbLineW : cbLineN;
+  unsigned long const cbInBuff  = 4096;
+  unsigned long const cbLineLen = 120;
+  unsigned long const cbLineW   = 32;
+  unsigned long const cbLineN   = 16;
+  unsigned long       cbIn;
+  unsigned long       nBuffOffset = 0;
+  unsigned long       cbLine = bWide ? cbLineW : cbLineN;
 
-    char *Line   = new char[ cbLineLen ];
-    char *InBuff = new char[ cbInBuff ];
+  char *Line   = new char[ cbLineLen ];
+  char *InBuff = new char[ cbInBuff ];
 
-    memset( InBuff, 0, sizeof( InBuff ) );
+  memset( InBuff, 0, cbInBuff );
 
-    do
+  do
+  {
+    unsigned long nRecords;
+    char         *InLine = InBuff;
+    unsigned long rec;
+    unsigned long nOffset;
+    unsigned long nSpace = 0;
+    unsigned long nSpaceCtl = cbLineW * 8 / cbLine;
+
+//    FTarget.read( InBuff, cbInBuff );
+    std::cin.read( InBuff, cbInBuff );
+//    cbIn = FTarget.gcount();
+    cbIn = std::cin.gcount();
+
+    nRecords = ( cbIn + cbLine - 1 ) / cbLine;
+
+    for ( rec = 0, nOffset = 0; rec < nRecords; rec++, nOffset += cbLine, InLine += cbLine, nBuffOffset += cbLine )
     {
-        unsigned long nRecords;
-        char         *InLine = InBuff;
-        unsigned long rec;
-        unsigned long nOffset;
-        unsigned long nSpace = 0;
-        unsigned long nSpaceCtl = cbLineW * 8 / cbLine;
+      char          Offset[ 12 ] = "";
+      unsigned long current      = nOffset + cbLine;
+      unsigned long nRecord      = current < cbIn ? cbLine : cbLine - ( current - cbIn );
 
-        FTarget.read( InBuff, cbInBuff );
-        cbIn = FTarget.gcount();
+//      FOut << "  "
+      std::cout << "  "
+           << FormatOffset( Offset, nBuffOffset )
+           << "  "
+           << FormatOctet( InLine, Line, nRecord, bWide )
+           << std::endl
+           ;
 
-        nRecords = ( cbIn + cbLine - 1 ) / cbLine;
+      if ( ++nSpace == nSpaceCtl )
+      {
+        nSpace = 0;
+//        FOut << '\n';
+        std::cout << '\n';
+      }
+    }
+  } while ( cbIn == cbInBuff );
 
-        for ( rec = 0, nOffset = 0; rec < nRecords; rec++, nOffset += cbLine, InLine += cbLine, nBuffOffset += cbLine )
-        {
-            char          Offset[ 12 ] = "";
-            unsigned long current      = nOffset + cbLine;
-            unsigned long nRecord      = current < cbIn ? cbLine : cbLine - ( current - cbIn );
+  delete [] Line;
+  delete [] InBuff;
 
-            FOut << "  "
-                 << FormatOffset( Offset, nBuffOffset )
-                 << "  "
-                 << FormatOctet( InLine, Line, nRecord, bWide )
-                 << endl
-                 ;
-
-            if ( ++nSpace == nSpaceCtl )
-            {
-                nSpace = 0;
-                FOut << '\n';
-            }
-        }
-    } while ( cbIn == cbInBuff );
-
-    delete [] Line;
-    delete [] InBuff;
-
-    switch ( argcLocal )
-    {
+  switch ( argcLocal )
+  {
     default:
     case 4:
-        buffError.close();
+//      buffError.close();
+      std::cerr.rdbuf(cerrbuf);
 
     case 3:
-        buffOut.close();
+//      buffOut.close();
+      std::cout.rdbuf(coutbuf);
 
     case 2:
-        buffTarget.close();
+//      buffTarget.close();
+      std::cin.rdbuf(cinbuf);
 
     case 1:
     case 0:
-        break;
-    }
+      break;
+  }
 
-    return 0;
+  return 0;
 }
 
+//  MARK: spitUsage()
 // ----------------------------------------------------------------------------
 // Function name    : spitUsage
 // Description      :
@@ -218,103 +238,105 @@ int main( int argc, char **argv, char **envp )
 // Argument         : ostream &out
 // Exceptions       :
 // ----------------------------------------------------------------------------
-void spitUsage( ostream &out )
+void spitUsage( std::ostream &out )
 {
-    out << "Usage:  prdump <switches> inFile outFile errorLog"
-        << '\n'
-        << "\twhere:  inFile is the file to read.  (default stdin.)"
-        << '\n'
-        << "\t        outFile is where the results are written.  (default stdout.)"
-        << '\n'
-        << "\t        errorLog is where the error messages are written.  (default stderr.)"
-        << '\n'
-        << '\n'
-        << "\tswitches:"
-        << '\n'
-        << "\t\t-h\tHelp.  This message."
-        << '\n'
-        << "\t\t-?\tDitto."
-        << '\n'
-        << "\t\t-n\tPrint the report in narrow format."
-        << '\n'
-        << "\t\t-w\tPrint the report in wide format.  (default.)"
-        << endl
-        ;
+  out << "Usage:  prdump <switches> inFile outFile errorLog"
+      << '\n'
+      << "\twhere:  inFile is the file to read.  (default stdin.)"
+      << '\n'
+      << "\t        outFile is where the results are written.  (default stdout.)"
+      << '\n'
+      << "\t        errorLog is where the error messages are written.  (default stderr.)"
+      << '\n'
+      << '\n'
+      << "\tswitches:"
+      << '\n'
+      << "\t\t-h\tHelp.  This message."
+      << '\n'
+      << "\t\t-?\tDitto."
+      << '\n'
+      << "\t\t-n\tPrint the report in narrow format."
+      << '\n'
+      << "\t\t-w\tPrint the report in wide format.  (default.)"
+      << std::endl
+      ;
 
-    return;
+  return;
 }
 
+//  MARK: FormatOctet()
 // ----------------------------------------------------------------------------
 // Function name    : *FormatOctet
 // Description      :
 // Return type      : char
-// Argument         :  char *iBuf
+// Argument         : char *iBuf
 // Argument         : char *oBuf
 // Argument         : int nChar
 // Argument         : boolean bWide
 // Exceptions       :
 // ----------------------------------------------------------------------------
-char *FormatOctet( char *iBuf, char *oBuf, int nChar, boolean bWide )
+char *FormatOctet( char *iBuf, char *oBuf, size_t nChar, boolean bWide )
 {
-    // Wide...
-    // 0....+....1....+....2....+....3....+....4....+....5....+....6....+....7....+....8....+....9....+....0....+....1
-    // 00010203 04050607 08090a0b 0c0d0e0f  10111213 14151617 18191a1b 1c1d1e1f    0123456789abcdef 0123456789abcdef
+  // Wide...
+  // 0....+....1....+....2....+....3....+....4....+....5....+....6....+....7....+....8....+....9....+....0....+....1
+  // 00010203 04050607 08090a0b 0c0d0e0f  10111213 14151617 18191a1b 1c1d1e1f    0123456789abcdef 0123456789abcdef
 
-    // Narrow...
-    // 0....+....1....+....2....+....3....+....4....+....5....+....6....+....7....+....8....+....9....+....0....+....1
-    // 00010203 04050607 08090a0b 0c0d0e0f  0123456789abcdef
+  // Narrow...
+  // 0....+....1....+....2....+....3....+....4....+....5....+....6....+....7....+....8....+....9....+....0....+....1
+  // 00010203 04050607 08090a0b 0c0d0e0f  0123456789abcdef
 
-    unsigned int const cbLine = 110;
+  unsigned int const cbLine = 110;
 
-    unsigned int   nPOffset = bWide ? 76 : 37;
-    unsigned char *iThis    = reinterpret_cast< unsigned char * >( iBuf );
-    unsigned char *oThisX   = reinterpret_cast< unsigned char * >( oBuf );
-    unsigned char *oThisP   = reinterpret_cast< unsigned char * >( oBuf + nPOffset );
-    short register spacer1  = 0;
-    short register spacer2  = 0;
-    unsigned char  Text;
-    unsigned char  Work[ 3 ];
+  unsigned int   nPOffset = bWide ? 76 : 37;
+  unsigned char *iThis    = reinterpret_cast< unsigned char * >( iBuf );
+  unsigned char *oThisX   = reinterpret_cast< unsigned char * >( oBuf );
+  unsigned char *oThisP   = reinterpret_cast< unsigned char * >( oBuf + nPOffset );
+  short spacer1           = 0;
+  short spacer2           = 0;
+  unsigned char Text;
+  unsigned char Work[ 3 ];
 
-    memset( oBuf, ' ', cbLine );
+  memset( oBuf, ' ', cbLine );
 
-    for ( register int i = 0; i < nChar; i++ )
+  for ( size_t i = 0; i < nChar; i++ )
+  {
+    Text = *iThis++;
+
+    sprintf( reinterpret_cast< char * >( Work ), "%02x", Text );
+    memcpy( oThisX, Work, 2 );
+
+    if ( isprint( Text ) || isgraph( Text ) )
     {
-        Text = *iThis++;
-
-        sprintf( reinterpret_cast< char * >( Work ), "%02x", Text );
-        memcpy( oThisX, Work, 2 );
-
-        if ( isprint( Text ) || isgraph( Text ) )
-        {
-            *oThisP = Text;
-        }
-        else
-        {
-            *oThisP = '.';
-        }
-
-        oThisX += 2;
-        oThisP++;
-
-        if ( ++spacer1 == 4 )
-        {
-            spacer2++;
-            spacer1 = 0;
-            oThisX++;
-        }
-
-        if ( spacer2 == 4 )
-        {
-            spacer2 = 0;
-            oThisX++;
-        }
+      *oThisP = Text;
+    }
+    else
+    {
+      *oThisP = '.';
     }
 
-    *oThisP = '\0';
+    oThisX += 2;
+    oThisP++;
 
-    return oBuf;
+    if ( ++spacer1 == 4 )
+    {
+      spacer2++;
+      spacer1 = 0;
+      oThisX++;
+    }
+
+    if ( spacer2 == 4 )
+    {
+      spacer2 = 0;
+      oThisX++;
+    }
+  }
+
+  *oThisP = '\0';
+
+  return oBuf;
 }
 
+//  MARK: FormatOffset()
 // ----------------------------------------------------------------------------
 // Function name    : *FormatOffset
 // Description      :
@@ -325,11 +347,12 @@ char *FormatOctet( char *iBuf, char *oBuf, int nChar, boolean bWide )
 // ----------------------------------------------------------------------------
 inline char *FormatOffset( char *Offset, unsigned long nOffset )
 {
-    sprintf( Offset, "%08X", nOffset );
+  sprintf( Offset, "%08lX", nOffset );
 
-    return Offset;
+  return Offset;
 }
 
+//  MARK: CheckForSwitch()
 // ----------------------------------------------------------------------------
 // Function name    : CheckForSwitch
 // Description      :
@@ -339,39 +362,40 @@ inline char *FormatOffset( char *Offset, unsigned long nOffset )
 // ----------------------------------------------------------------------------
 inline boolean CheckForSwitch( char *Arg )
 {
-    boolean Result;
-    char    Switch = *Arg;
-    char   *Val    = Arg;
+  boolean Result;
+  char    Switch = *Arg;
+  char   *Val    = Arg;
 
-    switch( Switch )
-    {
+  switch( Switch )
+  {
     case '-':
     case '/':
-        switch ( toupper( *( ++Val ) ) )
-        {
+      switch ( toupper( *( ++Val ) ) )
+      {
         case '?':
         case 'H':
-          spitUsage( cerr );
+          spitUsage( std::cerr );
 
         case 'W':
         case 'N':
-            Result = true;
-            break;
+          Result = true;
+          break;
 
         default:
-            Result = false;
-            break;
-        }
-        break;
+          Result = false;
+          break;
+      }
+      break;
 
-    default:
-        Result = false;
-        break;
-    }
+  default:
+    Result = false;
+    break;
+  }
 
-    return Result;
+  return Result;
 }
 
+//  MARK: IsItNarrow()
 // ----------------------------------------------------------------------------
 // Function name    : IsItNarrow
 // Description      :
@@ -382,34 +406,35 @@ inline boolean CheckForSwitch( char *Arg )
 // ----------------------------------------------------------------------------
 inline boolean IsItNarrow( char *Arg, boolean State )
 {
-    boolean Result;
-    char    Switch = *Arg;
-    char   *Val    = ++Arg;
+  boolean Result;
+  char    __unused Switch = *Arg;
+  char   *Val    = ++Arg;
 
-    // State is set true by default.  A State of "false" implies a
-    // -n switch has already been processed.
-    if ( State )
-    {
-        switch ( toupper( *Val ) )
-        {
+  // State is set true by default.  A State of "false" implies a
+  // -n switch has already been processed.
+  if ( State )
+  {
+      switch ( toupper( *Val ) )
+      {
         case 'N':
-            Result = false;
-            break;
+          Result = false;
+          break;
 
         case 'W':
         default:
-            Result = true;
-            break;
-        }
-    }
-    else
-    {
-        Result = State;
-    }
+          Result = true;
+          break;
+      }
+  }
+  else
+  {
+    Result = State;
+  }
 
-    return Result;
+  return Result;
 }
 
+//  MARK: EnvSnoop()
 // ----------------------------------------------------------------------------
 // Function name    : EnvSnoop
 // Description      :
@@ -420,23 +445,23 @@ inline boolean IsItNarrow( char *Arg, boolean State )
 // ----------------------------------------------------------------------------
 inline void EnvSnoop( char *Arg, char **envp )
 {
-    char *Val    = ++Arg;
-    char *EnvVar;
+  char *Val    = ++Arg;
+  char *EnvVar;
 
-    switch ( toupper( *Val ) )
-    {
+  switch ( toupper( *Val ) )
+  {
     case 'E':
-        while ( *envp != NULL )
-        {
-            EnvVar = *envp++;
-        }
-        break;
+      while ( *envp != NULL )
+      {
+        EnvVar = *envp++;
+      }
+      break;
 
     default:
-        break;
-    }
+      break;
+  }
 
-    return;
+  return;
 }
 
 // ----------------------------------------------------------------------------
